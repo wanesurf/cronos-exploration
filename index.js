a470a2bcae306b8561419201912c45586847744d
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-07-02 22:58:05 -0400

    feat: Add getBalance for a specified CRC20 token

--- index.js
+++ index.js
@@ -31,6 +31,57 @@ async function eth_getBalance(address) {
   }
 }
 
+app.get("/balance/:address/:tokenAddress", async (req, res) => {
+  const address = req.params.address;
+  const tokenAddress = req.params.tokenAddress;
+  const { balance, symbol } = await cerc20_getBalance(address, tokenAddress);
+  res.json({
+    address: address,
+    balance: balance,
+    symbol: symbol,
+  });
+});
+
+async function cerc20_getBalance(address, tokenAddress) {
+  try {
+    const web3 = new Web3(cronosRpcMainnet);
+    const abi = [
+      {
+        constant: true,
+        inputs: [{ name: "_owner", type: "address" }],
+        name: "balanceOf",
+        outputs: [{ name: "balance", type: "uint256" }],
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "symbol",
+        outputs: [{ name: "", type: "string" }],
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "decimals",
+        outputs: [{ name: "", type: "uint8" }],
+        type: "function",
+      },
+    ];
+    const contract = new web3.eth.Contract(abi, tokenAddress);
+    const balance = await contract.methods.balanceOf(address).call();
+    console.log({ balance });
+    const symbol = await contract.methods.symbol().call();
+    const decimals = await contract.methods.decimals().call();
+    // applying the correct decimal as it can be different for each token
+    const formattedBalance = web3.utils.fromWei(balance, parseInt(decimals));
+
+    return { balance: formattedBalance, symbol: symbol };
+  } catch (error) {
+    console.log(`Failed to get balance for ${address}: ${error.message}`);
+  }
+}
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
